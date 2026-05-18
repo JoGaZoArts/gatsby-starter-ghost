1a27e3bc5dde9ed41546a2fda5100dd1cc77a339
JoGaZoArts <juanladinoxp@gmail.com>
2026-05-17 23:55:58 -0500

    pi

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,65 +1,61 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import { graphql } from "gatsby";
+import * as React from "react"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
 
-import { Layout, PostCard, Pagination } from "../components/common";
-import { MetaData } from "../components/common/meta";
+// Desde aquí la ruta sube un nivel (../) y entra a la carpeta común
+import { Layout, PostCard, Pagination } from "../components/common"
 
-/**
- * Main index page (home page)
- *
- * Loads all posts from Ghost and uses pagination to navigate through them.
- * The number of posts that should appear per page can be setup
- * in /utils/siteConfig.js under `postsPerPage`.
- *
- */
-const Index = ({ data, location, pageContext }) => {
-    const posts = data.allGhostPost.edges;
+const Index = ({ data, pageContext }) => {
+    // Tomamos los posts que vienen desde Ghost
+    const posts = data.allGhostPost.edges
 
     return (
-        <>
-            <MetaData location={location} />
-            <Layout isHome={true}>
-                <div className="container">
-                    <section className="post-feed">
-                        {posts.map(({ node }) => (
-                            // The tag below includes the markup for each post - components/common/PostCard.js
-                            <PostCard key={node.id} post={node} />
-                        ))}
-                    </section>
-                    <Pagination pageContext={pageContext} />
-                </div>
-            </Layout>
-        </>
-    );
-};
+        <Layout isHome={true}>
+            <div className="container">
+                {/* Encabezado limpio y minimalista para el ejercicio de la UNIR */}
+                <section style={{ padding: "40px 0", textAlign: "center" }}>
+                    <h2 style={{ fontSize: "2.2rem", marginBottom: "10px", fontWeight: "700" }}>
+                        Entradas del Blog
+                    </h2>
+                    <p style={{ color: "#666", maxWidth: "600px", margin: "0 auto 30px auto", fontSize: "1.1rem" }}>
+                        Bienvenido. A continuación encontrarás las 3 entradas requeridas con texto e imágenes integradas.
+                    </p>
+                </section>
+
+                {/* El contenedor nativo que pinta las tarjetas del blog */}
+                <section className="post-feed">
+                    {posts.map(({ node }) => (
+                        <PostCard key={node.id} post={node} />
+                    ))}
+                </section>
+                
+                <Pagination pageContext={pageContext} />
+            </div>
+        </Layout>
+    )
+}
 
 Index.propTypes = {
     data: PropTypes.shape({
         allGhostPost: PropTypes.object.isRequired,
     }).isRequired,
-    location: PropTypes.shape({
-        pathname: PropTypes.string.isRequired,
-    }).isRequired,
     pageContext: PropTypes.object,
-};
+}
 
-export default Index;
+export default Index
 
-// This page query loads all posts sorted descending by published date
-// The `limit` and `skip` values are used for pagination
 export const pageQuery = graphql`
-    query GhostPostQuery($limit: Int!, $skip: Int!) {
-        allGhostPost(
-            sort: { order: DESC, fields: [published_at] }
-            limit: $limit
-            skip: $skip
-        ) {
-            edges {
-                node {
-                    ...GhostPostFields
-                }
-            }
+  query GhostPostQuery($limit: Int!, $skip: Int!) {
+    allGhostPost(
+        sort: { order: DESC, fields: [published_at] },
+        limit: $limit,
+        skip: $skip
+    ) {
+      edges {
+        node {
+          ...GhostPostFields
         }
+      }
     }
-`;
+  }
+`
